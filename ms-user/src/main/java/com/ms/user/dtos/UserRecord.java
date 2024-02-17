73bbfa668dd4422221b70dc8578a75f794ada1ff
valberjr <valber@duck.com>
2024-02-17 16:28:09 -0300

    save user endpoint

---
+++ ms-user/src/main/java/com/ms/user/dtos/UserRecord.java
@@ -0,0 +1,14 @@
+package com.ms.user.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserRecord(
+        @NotBlank
+        String name,
+
+        @NotBlank
+        @Email
+        String email
+) {
+}
