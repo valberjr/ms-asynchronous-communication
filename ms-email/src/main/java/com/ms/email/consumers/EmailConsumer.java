1ad412bb8473a1e783bbf8fbe7590dd91b3c3c70
valberjr <valber@duck.com>
2024-02-20 13:23:11 -0300

    send email implementation

--- ms-email/src/main/java/com/ms/email/consumers/EmailConsumer.java
+++ ms-email/src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -1,16 +1,25 @@
 package com.ms.email.consumers;
 
 import com.ms.email.dtos.EmailRecord;
+import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class EmailConsumer {
+
+    private final EmailService emailService;
 
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecord emailRecord) {
-        System.out.println(emailRecord.emailTo());
+        var emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailRecord, emailModel);
+        emailService.sendEmail(emailModel);
     }
 
 }
---
+++ ms-email/src/main/java/com/ms/email/services/EmailService.java
@@ -0,0 +1,46 @@
+package com.ms.email.services;
+
+import com.ms.email.enums.StatusEmail;
+import com.ms.email.models.EmailModel;
+import com.ms.email.repositories.EmailRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final EmailRepository emailRepository;
+    private final JavaMailSender emailSender;
+
+    @Value("${spring.mail.username}")
+    private String emailFrom;
+
+    @Transactional
+    public void sendEmail(EmailModel emailModel) {
+        try {
+            emailModel.setSendDateEmail(LocalDateTime.now());
+            emailModel.setEmailFrom(emailFrom);
+
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getMessage());
+
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException e) {
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            emailRepository.save(emailModel);
+        }
+    }
+}
